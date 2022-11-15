3a1ecd3923d8add6d516090a877a94a656723501
Saif Rashed <saif.rashed@hva.nl>
2022-11-15 23:19:29 +0100

    Added retro files

---
+++ app/src/main/java/com/ticketflip/scanner/data/api/util/Resource.kt
@@ -0,0 +1,15 @@
+package com.hva.amsix.data.api.util
+
+/**
+ * You can consider a sealed class as an enum++
+ * Sealed classes can also contain state - making it very useful for different network response
+ */
+sealed class Resource<T>(
+    val data: T? = null,
+    val message: String? = null
+) {
+    class Success<T>(data: T) : Resource<T>(data)
+    class Error<T>(message: String?, data: T? = null) : Resource<T>(data, message)
+    class Loading<T> : Resource<T>()
+    class Empty<T> : Resource<T>()
+}
