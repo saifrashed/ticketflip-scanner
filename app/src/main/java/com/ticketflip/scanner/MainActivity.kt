650ed40d76d637a03e4192c4cc71bbdae86bb939
Saif Rashed <saif.rashed@hva.nl>
2022-11-16 08:37:15 +0100

    Added NavHost and UIViewModel

--- app/src/main/java/com/ticketflip/scanner/MainActivity.kt
+++ app/src/main/java/com/ticketflip/scanner/MainActivity.kt
@@ -4,16 +4,25 @@ import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
+import androidx.compose.material.Scaffold
+import androidx.compose.material.ScaffoldState
+import androidx.compose.material.rememberScaffoldState
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
 import androidx.compose.runtime.SideEffect
+import androidx.compose.runtime.rememberCoroutineScope
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
-import androidx.compose.ui.tooling.preview.Preview
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.rememberNavController
 import com.google.accompanist.systemuicontroller.rememberSystemUiController
+import com.hva.amsix.util.Screen
+import com.ticketflip.scanner.ui.UIViewModel
 import com.ticketflip.scanner.ui.theme.TicketflipscannerTheme
+import kotlinx.coroutines.launch
 
 class MainActivity : ComponentActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
@@ -34,7 +43,7 @@ class MainActivity : ComponentActivity() {
                     modifier = Modifier.fillMaxSize(),
                     color = MaterialTheme.colorScheme.background
                 ) {
-                    Greeting("Android")
+                    TicketflipScannerApp()
                 }
             }
         }
@@ -42,14 +51,105 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-    Text(text = "Hello $name!")
+fun TicketflipScannerApp(UIViewModel: UIViewModel = androidx.lifecycle.viewmodel.compose.viewModel()) {
+    val navController = rememberNavController()
+
+    val scaffoldState: ScaffoldState = rememberScaffoldState()
+    val scope = rememberCoroutineScope()
+
+    NavHost(navController, UIViewModel, scaffoldState)
+
+    LaunchedEffect(key1 = true) {
+        UIViewModel.sharedFlow.collect { event ->
+            when (event) {
+                is UIViewModel.UIEvents.ShowSnackbar -> {
+                    scope.launch {
+                        scaffoldState.snackbarHostState.showSnackbar(
+                            event.message,
+                        )
+                    }
+                }
+                is UIViewModel.UIEvents.Navigate -> {
+                    navController.navigate(event.route)
+                }
+                is UIViewModel.UIEvents.GoBack -> {
+                    navController.popBackStack()
+                }
+            }
+        }
+    }
 }
 
-@Preview(showBackground = true)
+
 @Composable
-fun DefaultPreview() {
-    TicketflipscannerTheme {
-        Greeting("Android")
+private fun NavHost(
+    navController: NavHostController,
+    UIViewModel: UIViewModel,
+    scaffoldState: ScaffoldState
+) {
+
+    androidx.navigation.compose.NavHost(
+        navController,
+        startDestination = Screen.ScanAccessScreen.route,
+
+        ) {
+
+        // Access screen
+        composable(Screen.ScanAccessScreen.route) {
+            Scaffold(
+                scaffoldState = scaffoldState,
+                content = {
+                    /* TOOD */
+                }
+            )
+        }
+
+        // Event screen
+        composable(Screen.EventScreen.route) {
+            Scaffold(
+                scaffoldState = scaffoldState,
+                content = {
+                    /* TOOD */
+                }
+            )
+        }
+
+        // Profile screen
+        composable(Screen.ProfileScreen.route) {
+            Scaffold(
+                scaffoldState = scaffoldState,
+                content = {
+                    /* TOOD */
+                }
+            )
+        }
+
+        // Profile screen
+        composable(Screen.ProfileScreen.route) {
+            Scaffold(
+                scaffoldState = scaffoldState,
+                content = {
+                    /* TOOD */
+                }
+            )
+        }
+
+
+//        //Read Datapod screen
+//        composable(Screen.ReadDatapodScreen.route + "/{datapodId}") { navBackStackEntry ->
+//            /* Extracting the id from the route */
+//            val datapodId = navBackStackEntry.arguments?.getString("datapodId")
+//            /* We check if is null */
+//            datapodId?.let {
+//                AppShellDatapodRead("Datapod", UIViewModel, scaffoldState) {
+//                    ReadDatapodScreen(datapodId = datapodId)
+//                }
+//            }
+//
+//        }
+
+
+        // the rest...
     }
+
 }
---
+++ app/src/main/java/com/ticketflip/scanner/ui/UIViewModel.kt
@@ -0,0 +1,53 @@
+package com.ticketflip.scanner.ui
+
+import androidx.lifecycle.ViewModel
+import androidx.lifecycle.viewModelScope
+import kotlinx.coroutines.flow.MutableSharedFlow
+import kotlinx.coroutines.flow.MutableStateFlow
+import kotlinx.coroutines.flow.asSharedFlow
+import kotlinx.coroutines.flow.asStateFlow
+import kotlinx.coroutines.launch
+
+class UIViewModel : ViewModel() {
+
+    private val _bottomNavIndex = MutableStateFlow(0)
+    val bottomNavIndex = _bottomNavIndex.asStateFlow()
+
+    private val _sharedFlow = MutableSharedFlow<UIEvents>()
+    val sharedFlow = _sharedFlow.asSharedFlow()
+
+
+    fun navigate(route: String) {
+        viewModelScope.launch {
+            _sharedFlow.emit(UIEvents.Navigate(route))
+        }
+    }
+
+    fun goBack(activated: Boolean) {
+        viewModelScope.launch {
+            _sharedFlow.emit(UIEvents.GoBack(activated))
+        }
+    }
+
+
+    fun showSnackbar(message: String) {
+        viewModelScope.launch {
+            _sharedFlow.emit(UIEvents.ShowSnackbar(message))
+        }
+    }
+
+
+    fun clickBottomNavItem(index: Int) {
+        viewModelScope.launch {
+            _bottomNavIndex.value = UIEvents.ClickBottomNavItem(index).index
+
+        }
+    }
+
+    sealed class UIEvents {
+        data class ShowSnackbar(val message: String) : UIEvents()
+        data class Navigate(val route: String) : UIEvents()
+        data class GoBack(val activated: Boolean) : UIEvents()
+        data class ClickBottomNavItem(val index: Int) : UIEvents()
+    }
+}
